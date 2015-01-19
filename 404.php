f373969d5374562a34f63fd65433f6c71eb3af53
banrikun <i@banri.me>
2015-01-19 14:52:15 +0800

    add 404

---
+++ 404.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<title>BrDev</title>
+<style>
+body {
+    background: url(http://cdn.benyuanzu.com/images/bg.jpg) no-repeat 50% 0 fixed;
+}
+#wrap {
+    width: 340px;
+    height: 180px;
+    margin: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    border: 0;
+}
+#tip {
+    width: 340px;
+    height: 100px;
+    background: url(http://cdn.benyuanzu.com/images/tip.png) no-repeat;
+}
+#wrap a {
+    display: block;
+    box-sizing: border-box;
+    width: 320px;
+    line-height: 60px;
+    margin: 20px 10px 0;
+    border: 1px solid #fff;
+    color: #fff;
+    text-decoration: none;
+    text-align: center;
+    font-size: 24px;
+    font-family: \5FAE\8F6F\96C5\9ED1;
+}
+#wrap a:hover {
+    background: #fff;
+    color: #3f010c;
+}
+</style>
+</head>
+<body>
+    <div id="wrap">
+        <div id="tip"></div>
+        <a href="/" title="返回首页">返回首页</a>
+    </div>
+</body>
+</html>
