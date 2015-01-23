8cdf03fa3e42e2ecd18a784454fcafbb6f1d2eb1
banrikun <i@banri.me>
2015-01-24 01:33:58 +0800

    优化体验

--- common.js
+++ common.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $(".article:has(.thumb)").addClass("hasimg");
     $(".article:contains(\u005b\u89c6\u9891\u005d)").addClass("hasvid");
     $(".content a").attr("target","_blank");
+    $(".content a").not("[href*='benyuanzu.com']").attr("rel","nofollow");
 
     $("#menu").on("click",function() {
         $("#nav").toggle(300);
