095cbb548a0f0305c2cec3c162d1d6ee82c9df5a
banrikun <i@banri.me>
2015-01-24 18:02:12 +0800

    代码简化

--- common.js
+++ common.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
     $(".article:has(.thumb)").addClass("hasimg");
     $(".article:contains(\u005b\u89c6\u9891\u005d)").addClass("hasvid");
     $(".content a").attr("target","_blank");
