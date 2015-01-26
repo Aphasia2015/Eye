1bf5eaa2dbe51f2a2e2c8ac8230c38e5a633559f
banrikun <i@banri.me>
2015-01-26 22:57:37 +0800

    样式简化

--- footer.php
+++ footer.php
@@ -1,8 +1,3 @@
-    <?php if ($this->is('index')): ?>
-    <footer class="wrapper clearfix">
-        &copy; 2015 DN本源计划<br><a href="//typecho.org" target="_blank">Powered by Typecho</a>
-    </footer>
-    <?php endif; ?>
 </body>
 <script src="//ko.benyuanzu.com/source/jquery-2.1.3.min.js"></script>
 <script src="//ko.benyuanzu.com/source/common.min.js"></script>
