b92a1999380bb1835f444230a42a2ab928a860ec
banrikun <i@banri.me>
2015-01-26 22:29:00 +0800

    样式简化

--- footer.php
+++ footer.php
@@ -1,7 +1,5 @@
-    <footer class="clearfix">
-        <div class="wrapper">
-            &copy; 2015 DN本源计划<br>Powered by Typecho
-        </div>
+    <footer class="wrapper clearfix">
+        &copy; 2015 DN本源计划<br><a href="//typecho.org/" target="_blank">Powered by Typecho</a>
     </footer>
 </body>
 <script src="//ko.benyuanzu.com/source/jquery-2.1.3.min.js"></script>
