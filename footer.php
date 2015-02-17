1ff268d38295634826e0abe3af392b3df3e13584
banrikun <i@banri.me>
2015-02-17 11:30:07 +0800

    调整js位置

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-</body>
 <script src="<?php $this->options->themeUrl('static/jquery-2.1.3.min.js'); ?>"></script>
 <script src="<?php $this->options->themeUrl('static/common.min.js'); ?>"></script>
+</body>
 </html>
